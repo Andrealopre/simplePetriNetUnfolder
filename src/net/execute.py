e06cb3edaa49a44700b74d0081d22e6b2fe7029d
andreaJecko <a.loprete3@campus.unimib.it>
2026-03-25 23:48:26 +0100

    Added execute class, for now on hold

---
+++ src/net/execute.py
@@ -0,0 +1,16 @@
+from petri_net import PetriNet
+
+class Simulator:
+
+    def __init__(self, net):
+        self.net = net
+
+    def simulate(self):
+        print("Simple net simulation")
+
+
+    def enabledTransitions(self):
+        for 
+
+    def fire(self, transition):
+
